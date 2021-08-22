e9665a33fa0727ba394311560826d67003761788
Michael Wong <michaelzhwong@gmail.com>
2021-08-21 18:57:11 -0700

    Add base Navbar and setup basic tests

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+import Navbar from './components/Navbar';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#EDD8BB',
+        },
+        secondary: {
+            main: '#E2AA87',
+        },
+    },
+});
 
 function App() {
-  return (
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <ThemeProvider theme={theme}>
+            <Navbar />
+        </ThemeProvider>
     </div>
-  );
+    );
 }
 
 export default App;
--- src/components/Navbar.test.js
+++ src/components/Navbar.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Navbar from './Navbar';
 
-test('renders Navbar', () => {
-    render(<App />);
+test('renders learn react link', () => {
+    render(<Navbar />);
     expect(screen.getByText('Bobae')).toBeInTheDocument();
 });
